a8a25f187d87b19f6723c642e79b9cb2543735e5
Odei Riveiro Zafra <odei.riveiro@gmail.com>
2017-06-07 11:49:11 +0200

    Creadas variables de clase utilidadas

--- src/Chat.java
+++ src/Chat.java
@@ -9,3 +9,27 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.regex.Pattern;
 
+/**
+ * Clase que simula el funcionamiento de un Chat. 
+ * Se encarga de alternar funciones de Servidor y Cliente para que desde la 
+ * misma aplicación se puedan realizar y recibir peticiones simultáneas.
+ *
+ * @author Odei Riveiro Zafra
+ * @version 08.06.2016
+ */
+public class Chat {
+    /**
+     * Variable de tipo cadena usada para almacenar el HOST de Conexión.
+     */
+    protected static String HOST = "localhost";
+    /**
+     * Variable entera usada para almacenar el Puerto de Conexión.
+     */
+    protected static int Puerto = 1800;
+    /**
+     * Buffer usado para capturar por teclado las cadenas introducidas.
+     */
+    protected static BufferedReader br = 
+            new BufferedReader(new InputStreamReader(System.in));
+    
+}
